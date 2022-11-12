13953d8dc95cf0a78510f31c3fbcaf5af2c7188d
Kaiyang Zheng <kaiyang.zheng@gmail.com>
2022-11-12 16:08:23 -0500

    fixed bug in train_update

--- transit_api/update/train_update.py
+++ transit_api/update/train_update.py
@@ -37,6 +37,7 @@ def update_train_info(if_modified_since):
             trip_id = trip_data['id']
             trip_predictions, last_modified_prediction = ping_mbta_api(f'https://api-v3.mbta.com/predictions?filter[trip]={trip_id}', if_modified_since)
             for prediction in trip_predictions:
+                print(prediction['relationships']['stop']['data']['id'])
                 predict_stop = Stop.objects.get(id=prediction['relationships']['stop']['data']['id'])
                 predict_stop_serializer = StopSerializer(predict_stop)
                 predictions.append({
