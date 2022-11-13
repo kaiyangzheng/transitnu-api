c68c7e11f05eb1408ea9c3a27771cf627df73b05
Kaiyang Zheng <kaiyang.zheng@gmail.com>
2022-11-12 21:40:17 -0500

    added helper file to update

---
+++ transit_api/update/helpers.py
@@ -0,0 +1,8 @@
+stops, last_modified = ping_mbta_api('https://api-v3.mbta.com/stops', None)
+
+def get_parent_stop(stop_id):
+    global stops 
+    for stop in stops:
+        if stop['id'] == stop_id:
+            return stop['relationships']['parent_station']['data']['id']
+    return stop_id
--- transit_api/update/train_update.py
+++ transit_api/update/train_update.py
@@ -1,15 +1,7 @@
 from ..models import Train, Stop, Line  
 from ..serializers import StopSerializer, LineSerializer
 from .ping_mbta_api import ping_mbta_api
-
-stops, last_modified = ping_mbta_api('https://api-v3.mbta.com/stops', None)
-
-def get_parent_stop(stop_id):
-    global stops 
-    for stop in stops:
-        if stop['id'] == stop_id:
-            return stop['relationships']['parent_station']['data']['id']
-    return stop_id
+from .helpers import get_parent_stop
 
 
 def update_train_info(if_modified_since):
