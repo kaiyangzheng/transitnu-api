2f681e7655a2f39b4af51af4d4244ffd4e159609
Kaiyang Zheng <kaiyang.zheng@gmail.com>
2022-11-12 21:17:58 -0500

    changed stop to get parent station

--- transit_api/update/stop_update.py
+++ transit_api/update/stop_update.py
@@ -3,10 +3,16 @@ from ..serializers import LineSerializer
 from .ping_mbta_api import ping_mbta_api
 
 def update_stop_info(if_modified_since):
+    all_stops, last_modified = ping_mbta_api("https://api-v3.mbta.com/stops", None)
     stops, last_modified = ping_mbta_api("https://api-v3.mbta.com/stops?filter%5Broute_type%5D=0,1", if_modified_since)
     if (len(stops)) == 0:
         return last_modified
     for stop in stops:
+        parent_station = stop['relationships']['parent_station']['data']['id']
+        for s in all_stops:
+            if s['id'] == parent_station:
+                stop = s
+                break
         stop_id = stop['id']
         stop_info = stop['attributes']
         stop_db = Stop.objects.filter(id=stop_id)
